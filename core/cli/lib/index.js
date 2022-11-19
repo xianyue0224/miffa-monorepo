9e7bedd2f74986adf5577c1ebc6edaef325488e0
xianyue <xianyue0224@gmail.com>
2022-11-19 18:27:09 +0800

    📦 NEW: 脚手架执行流程检查Node版本号功能

--- core/cli/lib/index.js
+++ core/cli/lib/index.js
@@ -1,16 +1,35 @@
 'use strict';
 
+const semver = require("semver")
+const colors = require("colors/safe")
 const pkg = require("../package.json")
 const log = require("@miffa/log")
+const constant = require("./const")
 
 module.exports = core;
 
 function core() {
-    console.log("hello miffa!")
-    checkPkgVersion()
-    log.success("", "test")
+    try {
+        checkPkgVersion()
+        checkNodeVersion()
+    } catch (e) {
+        log.error(e.message)
+    }
+
+}
+
+function checkNodeVersion() {
+    // 获取当前版本号
+    const cur = process.version
+
+    // 比对最低版本号
+    const lowest = constant.LOWEST_NODE_VERSION
+
+    if (!semver.gte(cur, lowest)) {
+        throw new Error(colors.red(`miffa cli 需要安装 v${lowest} 以上版本的 Node.js`))
+    }
 }
 
 function checkPkgVersion() {
-    log.notice("", pkg.version)
+    log.notice("", `脚手架当前版本为 v${pkg.version}`)
 }
